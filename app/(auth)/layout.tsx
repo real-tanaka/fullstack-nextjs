c6bb5ee760a98137cff1eacb7d86c672077bcd7a
real-tanaka <realtanakashoko@gmail.com>
2023-02-06 16:55:23 +0200

    sidebar and components

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+
+const AuthRootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className={"w-full h-full flex items-center justify-center"}>
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+};
+
+export default AuthRootLayout;
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+
+const DashboardRootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className={"w-full h-full flex items-center justify-center"}>
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+};
+
+export default DashboardRootLayout;

d1f2bbf64bca05bac1ddd1f34dd6a1d483518fcf
real-tanaka <realtanakashoko@gmail.com>
2023-02-07 16:45:30 +0200

    sign in forms

---
+++ lib/auth.ts
@@ -0,0 +1,5 @@
+import bcrypt from 'bcrypt'
+
+export const hashPassword = (password) => bcrypt.hash(password, 10)
+export const comparePasswords = (plainTextPassword, hashedPassword) => bcrypt.compare(plainTextPassword, hashedPassword)
+
